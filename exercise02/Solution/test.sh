67fbf2f514c3b949578f1a28f913b0b75442db64
Philipp-p <Philipp-p@users.noreply.github.com>
2017-10-23 13:41:57 +0200

    reformat

--- exercise02/Solution/test.sh
+++ exercise02/Solution/test.sh
@@ -4,8 +4,8 @@ for i in $(seq 10)
 do
     for j in 10 100 500 1000 1500
     do
-#wall time in [s]; user mode cpu; kernel mode; major page fault; minor page fault; max resident set size;avg resident set size
-gtime -f "%e;%U;%S;%F;%R;%M;%t" $1 $j 2>> $1$j.csv
+        #wall time in [s]; user mode cpu; kernel mode; major page fault; minor page fault; max resident set size;avg resident set size
+        gtime -f "%e;%U;%S;%F;%R;%M;%t" $1 $j 2>> $1$j.csv
         echo "Done iteration $i for $j"
     done
 done
