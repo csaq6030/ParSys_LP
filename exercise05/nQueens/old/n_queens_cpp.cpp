2ef3d99270429d8d0da142cf30f4ab9c5be62d06
Philipp-p <Philipp-p@users.noreply.github.com>
2017-11-14 13:21:40 +0100

    added old nqueen run (4 and 8 Threads only) and overview

---
+++ exercise05/nQueens/old/n_queens_cpp.cpp
@@ -0,0 +1,52 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+
+#include "chrono_timer.h"
+
+using namespace std;
+
+uint32_t solution = 0;
+uint32_t* ptr_s = &solution;
+uint32_t N;
+
+void putQueen(uint32_t *queens, const uint32_t row, const uint32_t col) {
+    for (uint32_t i = 0; i < row; i++) {
+        // check same row and diagonal
+        if (queens[i] == col || queens[i] == col + row - i || queens[i] == col - row +i) {
+            return;
+        }
+    }
+    
+    //found a solution
+    if(row == N-1) {
+        //#pragma omp atomic
+        //solution++;
+        __sync_add_and_fetch(ptr_s, 1);
+    } else {
+        //put Queen
+        queens[row] = col;
+        
+        
+        for(uint32_t i = 0; i < N; i++) {
+            putQueen(queens, row+1, i);
+        }
+    }
+}
+
+uint32_t rec_n_queens() {
+    #pragma omp parallel for
+    for(uint32_t i = 0; i < N; i++) {
+        uint32_t *queens = (uint32_t*)calloc(N, sizeof(uint32_t));
+        putQueen(queens, 0, i);
+        free(queens);
+    }
+	return solution;
+}
+
+
+void setTest(uint32_t n){
+	N = n;
+	solution = 0;
+}
