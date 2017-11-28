179281d9b1502df2045c625fc5f6cf490ed45387
csaq6030 <chhong.lee@student.uibk.ac.at>
2017-11-28 10:02:32 +0100

    vectorization of 1D, 2/3D not working, indirection also no good

--- exercise07/with indirections/solver.h
+++ exercise07/with indirections/solver.h
@@ -7,18 +7,21 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-
+#include <vector>
 
 using namespace std;
 
 class Solver {
 public:
-	Wrapper &data;
+	Wrapper *data;
 	const float epsilon;
 	size_t iter;
 	const int size;   
 
-    Solver(Wrapper & d, const float epsilon);
+	vector<float*> indirectionA;
+	vector<float*> indirectionB;
+
+    Solver(Wrapper* d, const float epsilon);
 	void compute();
 	void showResult();
 private:
@@ -33,14 +36,23 @@ private:
     
 };
 
-Solver::Solver(Wrapper & d, const float epsilon) :data(d), epsilon(epsilon), size(data.size){
-	
+Solver::Solver(Wrapper * d, const float epsilon) :data(d), epsilon(epsilon), size(data->size){
+	if(data->dim == 2){
+		for(unsigned i = 0; i < size+2; ++i) {
+		indirectionA.resize(size+2);
+		indirectionB.resize(size+2);
+			for(unsigned j = 0; j < size+2; ++j) {
+				indirectionA[i] = &data->arrayA[i*(size+2)];
+				indirectionB[i] = &data->arrayB[i*(size+2)];
+			}
+		}
+	}
 }
 
 void Solver::compute(){
 	float progress = 500000;
 	iter=0;
-	switch(data.dim){
+	switch(data->dim){
 		case 1:
 			while(progress > epsilon){
 				if(iter%2 == 0){
@@ -78,9 +90,9 @@ void Solver::compute(){
 
 void Solver::showResult(){
 	if(iter%2 == 0)
-		data.printA();
+		data->printA();
 	else
-		data.printB();
+		data->printB();
 	cout << "Iterations = " << iter << endl;
 }
 
@@ -88,9 +100,9 @@ float Solver::iteration1D_A(){
 	float prog = 0;
 	#pragma omp parallel for simd reduction(+: prog)
 	for(int i = 1; i < size + 1; i++){
-		float tmp = (data.arrayA[i-1] + data.arrayA[i] + data.arrayA[i+1])/3;
-		prog += abs(data.arrayA[i]-tmp);
-		data.arrayB[i] = tmp;
+		float tmp = (data->arrayA[i-1] + data->arrayA[i] + data->arrayA[i+1])/3;
+		prog += abs(data->arrayA[i]-tmp);
+		data->arrayB[i] = tmp;
 	}
 	return prog;
 }
@@ -99,22 +111,21 @@ float Solver::iteration1D_B(){
 	float prog = 0;
 	#pragma omp parallel for simd reduction(+: prog)
 	for(int i = 1; i < size + 1; i++){
-		float tmp = (data.arrayB[i-1] + data.arrayB[i] + data.arrayB[i+1])/3;
-		prog += abs(data.arrayB[i]-tmp);
-		data.arrayA[i] = tmp;
+		float tmp = (data->arrayB[i-1] + data->arrayB[i] + data->arrayB[i+1])/3;
+		prog += abs(data->arrayB[i]-tmp);
+		data->arrayA[i] = tmp;
 	}
 	return prog;
 }
 
 float Solver::iteration2D_A(){
 	float prog = 0;
-	#pragma omp parallel for reduction(+: prog) 
+	#pragma omp parallel for simd reduction(+: prog)
 	for(int i = 1; i < size + 1; i++){
 		for(int j = 1; j < size + 1; j++){
-			float tmp = (data.arrayA[access(i, j-1)] + data.arrayA[access(i, j)] + data.arrayA[access(i, j+1)] +
-						 data.arrayA[access(i-1, j)] + data.arrayA[access(i+1, j)])/5;
-			prog += abs(data.arrayA[access(i, j)]-tmp);
-			data.arrayB[access(i, j)] = tmp;
+			float tmp = (indirectionA[i][j-1] + indirectionA[i][j] + indirectionA[i][j+1] + indirectionA[i-1][j] + indirectionA[i+1][j])/5;
+			prog += abs(indirectionA[i][j]-tmp);
+			data->arrayB[access(i, j)] = tmp;
 		}
 	}
 	return prog;
@@ -122,13 +133,12 @@ float Solver::iteration2D_A(){
 
 float Solver::iteration2D_B(){
 	float prog = 0;
-	#pragma omp parallel for reduction(+: prog) 
+	#pragma omp parallel for simd reduction(+: prog)
 	for(int i = 1; i < size + 1; i++){
 		for(int j = 1; j < size + 1; j++){
-			float tmp = (data.arrayB[access(i, j-1)] + data.arrayB[access(i, j)] + data.arrayB[access(i, j+1)] +
-						 data.arrayB[access(i-1, j)] + data.arrayB[access(i+1, j)])/5;
-			prog += abs(data.arrayB[access(i, j)]-tmp);
-			data.arrayA[access(i, j)] = tmp;
+			float tmp = (indirectionB[i][j-1] + indirectionB[i][j] + indirectionB[i][j+1] + indirectionB[i-1][j] + indirectionB[i+1][j])/5;
+			prog += abs(indirectionB[i][j]-tmp);
+			data->arrayA[access(i, j)] = tmp;
 		}
 	}
 	return prog;
@@ -140,10 +150,10 @@ float Solver::iteration3D_A(){
 	for(int i = 1; i < size + 1; i++){
 		for(int j = 1; j < size + 1; j++){
 			for(int k = 1; k < size + 1; k++){
-				float tmp = (data.arrayB[access(i-1, j, k)] + data.arrayB[access(i+1, j, k)] + data.arrayB[access(i, j, k)] +
-							 data.arrayB[access(i, j-1, k)] + data.arrayB[access(i, j+1, k)] + data.arrayB[access(i, j, k-1)] + data.arrayB[access(i, j, k+1)] )/7;
-				prog += abs(data.arrayB[access(i, j, k)]-tmp);
-				data.arrayA[access(i, j, k)] = tmp;
+				float tmp = (data->arrayB[access(i-1, j, k)] + data->arrayB[access(i+1, j, k)] + data->arrayB[access(i, j, k)] +
+							 data->arrayB[access(i, j-1, k)] + data->arrayB[access(i, j+1, k)] + data->arrayB[access(i, j, k-1)] + data->arrayB[access(i, j, k+1)] )/7;
+				prog += abs(data->arrayB[access(i, j, k)]-tmp);
+				data->arrayA[access(i, j, k)] = tmp;
 			}			
 		}
 	}
@@ -156,10 +166,10 @@ float Solver::iteration3D_B(){
 	for(int i = 1; i < size + 1; i++){
 		for(int j = 1; j < size + 1; j++){
 			for(int k = 1; k < size + 1; k++){
-				float tmp = (data.arrayA[access(i-1, j, k)] + data.arrayA[access(i+1, j, k)] + data.arrayA[access(i, j, k)] +
-							 data.arrayA[access(i, j-1, k)] + data.arrayA[access(i, j+1, k)] + data.arrayA[access(i, j, k-1)] + data.arrayA[access(i, j, k+1)] )/7;
-				prog += abs(data.arrayA[access(i, j, k)]-tmp);
-				data.arrayB[access(i, j, k)] = tmp;
+				float tmp = (data->arrayA[access(i-1, j, k)] + data->arrayA[access(i+1, j, k)] + data->arrayA[access(i, j, k)] +
+							 data->arrayA[access(i, j-1, k)] + data->arrayA[access(i, j+1, k)] + data->arrayA[access(i, j, k-1)] + data->arrayA[access(i, j, k+1)] )/7;
+				prog += abs(data->arrayA[access(i, j, k)]-tmp);
+				data->arrayB[access(i, j, k)] = tmp;
 			}			
 		}
 	}
