119768507ee347e8ff083ec317a4c83784eb6511
csaq6030 <chhong.lee@student.uibk.ac.at>
2017-12-04 15:12:15 +0100

    monteCarlo

---
+++ exercise08/monteCarlo/monteCarloPi.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <omp.h>
+#include <mpi.h>
+
+#include "chrono_timer.h"
+
+using namespace std;
+
+double MCS_Seq(unsigned long n){
+	unsigned long c = 0;
+	double x, y;
+	random_device rd;	// random seed
+	mt19937 gen(rd()); 	// random generator
+	uniform_real_distribution<double> dis(0, 1.0);
+	for(unsigned i = 0; i < n; i++){
+		x = dis(gen);
+		y = dis(gen);
+		if(sqrt(x*x + y*y) <= 1)
+			c++;
+	}
+	return (double) c/n*4.0;
+}
+
+
+
+int main(int argc, char** argv) {
+
+	if(argc != 2) return EXIT_FAILURE;
+	unsigned long long n = atoi(argv[1]);
+	if(n == 0) return EXIT_FAILURE;
+
+	unsigned long c = 0;
+	double pi;
+	int myid;
+	int size;
+	int subIter;
+	int reducedCount;
+	double x, y;
+	MPI_Init(NULL, NULL);					//Start MPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);	//get rank/id of process
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	subIter = n/(unsigned long)size;
+
+	random_device rd;
+	mt19937 gen(rd() + myid); 
+	uniform_real_distribution<double> dis(0, 1.0);
+	int i;
+	if(myid == 0){
+		
+		for (i=0; i<subIter + n%size; ++i){
+			x = dis(gen);
+			y = dis(gen);
+			if(sqrt(x*x + y*y) <= 1)
+				c++;	
+		}	
+	}else{
+		for (i=0; i<subIter; ++i){
+			x = dis(gen);
+			y = dis(gen);
+			if(sqrt(x*x + y*y) <= 1)
+				c++;	
+		}
+	}
+	cout << "procces id: " << myid << "   iter: " << i << endl;
+	MPI_Reduce(&c, &reducedCount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Finalize(); 
+	
+	if (myid == 0){
+		pi = ((double)reducedCount/(double)n)*4.0;		
+		cout.precision(11);
+		cout << fixed;
+		cout<< "Estimated value of pi is: " << pi << endl;
+		cout<< "Actual    Value of pi is: 3.14159265359" << endl;
+	}
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
