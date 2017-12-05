010d4b1e2781a9ab29037fda552cd71c8f8b940b
Philipp-p <Philipp-p@users.noreply.github.com>
2017-12-05 03:08:34 +0100

    works now more or less -np 2 and 4 work fine, 3 is a bit of a gamble between fine results or a maloc error
    detail malloc error:
    incorrect checksum for freed object - object was probably modified
    after being freed

--- exercise08/stencil_mpi/main.cpp
+++ exercise08/stencil_mpi/main.cpp
@@ -226,7 +226,7 @@ int main(int argc, char* argv[]) {
                         MPI_Recv(tmp1, size * blockSize, MPI_FLOAT, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                         for(int i = 0; i < size; i++){
                             for (int j = 0; j < blockSize; j++) {
-                                result[i * size + j + b * blockSize] = tmp1[i * blockSize * m + j];
+                                result[i * size + j + b * blockSize] = tmp1[i * blockSize + j];
                             }
                         }
                     }
@@ -351,7 +351,7 @@ int main(int argc, char* argv[]) {
                         }
                     }
                     
-                    
+                    /*
                     for (int i = 0; i < n; i++) {
                         for (int j = 0; j < m; j++) {
                             cout << arrayA[i * m + j] << " ";
@@ -366,7 +366,7 @@ int main(int argc, char* argv[]) {
                         }
                         cout << endl;
                     }
-                    cout << endl;
+                    cout << endl; */
                     
                     
                     MPI_Send(tmp, (blockSize + size % worldSize) * size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
@@ -451,10 +451,26 @@ int main(int argc, char* argv[]) {
                     float *tmp = new float[size * blockSize]();
                     
                     for (int i = 1; i < n - 1; i++) {
-                        for (int j = 2; j < m - 2; j++) {
-                            tmp[(i - 1) * size + (j - 1)] = arrayA[i * m + j];
+                        for (int j = 2; j < m - 1; j++) {
+                            tmp[(i - 1) * blockSize + (j - 2)] = arrayA[i * m + j];
                         }
                     }
+                    
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                            cout << arrayA[i * m + j] << " ";
+                        }
+                        cout << endl;
+                    }
+                    cout << endl;
+                    
+                    for (int i = 0; i < size; i++) {
+                        for (int j = 0; j < blockSize ; j++) {
+                            cout << tmp[i * blockSize + j] << " ";
+                        }
+                        cout << endl;
+                    }
+                    cout << endl;
                     
                     MPI_Send(tmp, blockSize * size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
                     
